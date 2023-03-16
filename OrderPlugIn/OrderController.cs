5e574a92835bec68fe3df7c124afcdc1bd67b9e2
Remco Brosky <remcobrosky@microsoft.com>
2023-03-16 12:39:44 +0100

    roundtrip REST call (from one plugin to the other)

--- OrderPlugIn/OrderController.cs
+++ OrderPlugIn/OrderController.cs
@@ -2,13 +2,19 @@
 using Microsoft.Extensions.Logging;
 using SharedDomain;
 using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace OrderPlugIn {
     [ApiController]
     [Route("[controller]")]
     public class OrderController : ControllerBase {
         private readonly ILogger _logger;
+        private readonly string _baseAddress = "https://localhost:5001";
 
         public OrderController(ILogger<OrderController> logger) {
             _logger = logger;
@@ -16,14 +22,13 @@ namespace OrderPlugIn {
 
         [HttpGet]
         [Route("{OrderId}")]
-        public IActionResult GetOrder(int OrderId) {
-            Customer customer = new Customer();
-            customer.Id = 1;
-            customer.Name = "Lely";
+        public async Task<IActionResult> GetOrder(int OrderId) {
+            
 
             Order order = new Order();
             order.OrderNr = OrderId;
-            order.Customer = customer;
+            _logger.LogInformation("Getting customer for order {OrderId}", OrderId);
+            order.Customer = await GetCustomer(OrderId);
 
             return Ok(order);
         }
@@ -33,6 +38,19 @@ namespace OrderPlugIn {
             var newtonSoftJsonType = typeof(Newtonsoft.Json.JsonConvert).Assembly;
             return Ok("This plugin uses " + newtonSoftJsonType.GetName().ToString());
 
+        }
+
+
+        private async Task<Customer> GetCustomer(int Id) {
+            using (var client = new HttpClient()) {
+                client.BaseAddress = new Uri(_baseAddress);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync($"/customer/{Id}").Result;
+
+                string json = await response.Content.ReadAsStringAsync();
+                Customer customer = JsonConvert.DeserializeObject<Customer>(json);
+                return customer;
+            }
         }
 
     }
